13a1c0b9a3b6811f218a785c17a25507cbffd351
Julien WITTOUCK <julien.wittouck@gmail.com>
2024-05-03 20:37:59 +0200

    📝 : add variable types

--- variables.tf
+++ variables.tf
@@ -1,18 +1,22 @@
 variable "instance_name" {
   description = "name of the documentdb instance"
+  type        = string
 }
 
 variable "instance_type" {
   description = "type of the node of the instance"
+  type        = string
   default     = "DB-DEV-S"
 }
 
 variable "activate_ha" {
   description = "set to true to activate HA for this instance"
+  type        = bool
   default     = false
 }
 
 variable "disable_backup" {
   description = "set to false to disable backups for this instance"
+  type        = bool
   default     = false
 }
